be1eb3b44459164ee88c5b6c60cec3b2cdeea532
李欣 <JustinDellAdam@live.com>
2018-08-17 19:28:45 +0800

    version 1.3.0
    added indent switch

--- nester_lixin/nester.py
+++ nester_lixin/nester.py
@@ -4,20 +4,22 @@ which prints lists that may or may not include nested lists.
 """
 
 
-def print_lol(the_list, level):
+def print_lol(the_list, indent=False, level=0):
     """
     This function takes one positional argument called "the_list",
     which is any Python list (of - possibly - nested lists).
     Each data item in the provided list is (recursively) printed to the screen on it's own line.
 
     :param the_list: The list you would like to print
-    :param level: how many tabs before every level of the list
+    :param indent: Printing with indent or not
+    :param level: How many tab indents before every level of the list
     :return:
     """
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, level + 1)
+            print_lol(each_item, indent, level + 1)
         else:
-            for tab_stop in range(level):
-                print("\t", end='')
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
             print(each_item)
--- test.py
+++ test.py
@@ -5,3 +5,4 @@ from nester_lixin import nester
 if __name__ == '__main__':
     print(sys.path)
     nester.print_lol(sys.path)
+    nester.print_lol(sys.path, True, 1)
