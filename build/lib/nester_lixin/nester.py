400768ae717872a98c8124e717003666092bda72
李欣 <JustinDellAdam@live.com>
2018-08-18 17:13:23 +0800

    v 1.4.0
    new Function: print to file

--- build/lib/nester_lixin/nester.py
+++ build/lib/nester_lixin/nester.py
@@ -2,9 +2,10 @@
 This is the "nester.py" module and it provides one function called print_lol()
 which prints lists that may or may not include nested lists.
 """
+import sys
 
 
-def print_lol(the_list, indent=False, level=0):
+def print_lol(the_list, indent=False, level=0, fn=sys.stdout):
     """
     This function takes one positional argument called "the_list",
     which is any Python list (of - possibly - nested lists).
@@ -13,13 +14,14 @@ def print_lol(the_list, indent=False, level=0):
     :param the_list: The list you would like to print
     :param indent: Printing with indent or not
     :param level: How many tab indents before every level of the list
-    :return:
+    :param fn: The file name you would like to print to, default prints to the screen
+    :return: null
     """
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, indent, level + 1)
+            print_lol(each_item, indent, level + 1, fn)
         else:
             if indent:
                 for tab_stop in range(level):
-                    print("\t", end='')
-            print(each_item)
+                    print("\t", end='', file=fn)
+            print(each_item, file=fn)
