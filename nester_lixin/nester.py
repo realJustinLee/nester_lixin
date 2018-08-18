45ac6a353738f65774070909dd56e7b1bc303dee
李欣 <JustinDellAdam@live.com>
2018-08-18 17:43:49 +0800

    v1.4.1
    unify APIs

--- nester_lixin/nester.py
+++ nester_lixin/nester.py
@@ -5,7 +5,7 @@ which prints lists that may or may not include nested lists.
 import sys
 
 
-def print_lol(the_list, indent=False, level=0, fn=sys.stdout):
+def print_lol(the_list, indent=False, level=0, file=sys.stdout):
     """
     This function takes one positional argument called "the_list",
     which is any Python list (of - possibly - nested lists).
@@ -14,14 +14,14 @@ def print_lol(the_list, indent=False, level=0, fn=sys.stdout):
     :param the_list: The list you would like to print
     :param indent: Printing with indent or not
     :param level: How many tab indents before every level of the list
-    :param fn: The file name you would like to print to, default prints to the screen
+    :param file: The file name you would like to print to, default prints to the screen
     :return: null
     """
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, indent, level + 1, fn)
+            print_lol(each_item, indent, level + 1, file)
         else:
             if indent:
                 for tab_stop in range(level):
-                    print("\t", end='', file=fn)
-            print(each_item, file=fn)
+                    print("\t", end='', file=file)
+            print(each_item, file=file)
